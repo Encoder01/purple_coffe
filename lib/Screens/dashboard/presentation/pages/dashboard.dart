f4066dce16796ca2e9c2f9cce59da4b5fb3b3a2b
huzeyfe <jungar.kagan@gmail.com>
2022-05-03 14:26:10 +0300

    dashboard

--- lib/Screens/dashboard/presentation/pages/dashboard.dart
+++ lib/Screens/dashboard/presentation/pages/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
 class DashBoard extends StatelessWidget {
   const DashBoard({Key? key}) : super(key: key);
@@ -6,7 +7,26 @@ class DashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(height: 5,),
+          Text("Mor Fincan"),
+          Text("fotoğrafta belirtildiği gibi fotoları çekin"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Ionicons.cube)),
+              IconButton(onPressed: () {}, icon: Icon(Ionicons.cube)),
+              IconButton(onPressed: () {}, icon: Icon(Ionicons.cube)),
+              IconButton(onPressed: () {}, icon: Icon(Ionicons.cube))
+            ],
+          ),
+          Text("Artık tabak resmi çekebilirsiniz"),
+          IconButton(onPressed: () {}, icon: Icon(Ionicons.cube)),
+          TextButton(onPressed: (){}, child: Text("Gönder"))
+        ],
+      ),
     );
   }
 }
